60b8be8c529d96c3db7f617eeb0b12c1dd8786d8
prakhar29jain <prakhar29jain@gmail.com>
2023-05-10 17:42:39 +0530

    deploy trial

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,7 +32,7 @@ app.get("/home",function(req,res){
             // sort: { "imdb.rating": -1 },
             projection: { _id: 0},
             };
-    
+
             const cursor = candidateData.find(query, options);
             // print a message if no documents were found
             if ((await candidateData.countDocuments(query)) === 0) {
@@ -108,7 +109,61 @@ app.get("/candidate",function(req,res){
 
 app.get('/elections/:eleType',function(req,res){
     var  eleName = req.params.eleType;
-    res.render("election",{eleName:eleName});
+    console.log(eleName);
+    async function run() {
+        try {
+            const database = client.db("ElectionDB");
+            const Data = database.collection(eleName.replaceAll(/\s/g,''));
+            
+            // Query for a cadidate with parameters
+            const query = {};
+            const options = {
+            sort: { "votes": -1 },
+            projection: { _id: 0},
+            };
+    
+            const cursor = Data.find(query, options);
+
+            if((await Data.find({ 'votes' : { '$exists' : true }}).toArray()).length === 0){
+                const allValues = await cursor.toArray();
+                res.render("electionICom",{eleName:eleName,allValues:allValues});
+            }else{
+                const allValues = await cursor.toArray();
+                res.render("electionCom",{eleName:eleName,allValues:allValues});
+            }          
+        } finally {
+            //await client.close();
+        }
+        }
+        run().catch(console.dir);
+        
+});
+
+app.post("/elections/:eleType",function(req,res){
+    //console.log(req.originalUrl);
+    let eleName = req.params.eleType;
+    async function run() {
+        try {
+            const database = client.db("ElectionDB");
+            const Data = database.collection(eleName.replaceAll(/\s/g,''));
+    
+            // create a document to insert
+        const doc = {
+            name: req.body.name,
+            area: req.body.area,
+            gender: req.body.gender,
+            party: req.body.party,
+            calculatingVotes: 0
+            }
+    
+            const result = Data.insertOne(doc);
+            res.redirect("/elections/" + req.params.eleType);
+        } finally {
+            //await client.close();
+        }
+        }
+        run().catch(console.dir);
+   
 });
 
 
